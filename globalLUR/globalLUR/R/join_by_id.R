30e02b00497457fbb5d94737efc9d6441a773c1f
Lu <m.lu@uu.nl>
2018-11-29 10:54:37 +0100

    plot multiple

--- globalLUR/globalLUR/R/join_by_id.R
+++ globalLUR/globalLUR/R/join_by_id.R
@@ -1,6 +1,7 @@
-#' @param IDfile file with coordinate and ID that Oliver uses
-#' @param coordinatefile file with coordinate and variables values (air quality measurements) but no ID
-#' @param file2merge file to merge to, i.e. file with all the variables
+#' Join variables by ID, requires an ID file with coordinates
+#' @param IDfile sp file with coordinate and ID that Oliver uses
+#' @param coordinatefile sp file with coordinate and variables values (air quality measurements) but no ID
+#' @param file2merge dataframe to merge to, i.e. file with all the variables
 #' @param varname the name of the variable in the coordinatefile to merge
 #' @return a dataframe
 #' @export
@@ -11,12 +12,22 @@
 #meanoaq = read.csv("q_day.csv") #locations and values
 #testoaq = read.csv("openaqmorethan1000_ch_locations.csv") # ID with locaitons
 
-join_by_id= function(IDfile= testoaq, coordinatefile=meanoaq, file2merge = merged, varname="value")
+join_by_id= function(IDfile , coordinatefile, file2merge = merged, varname="value", newname)
 {
+  if(is.data.frame(IDfile)){
+    coordinates(IDfile) = ~longitude + latitude
+    warning("coordinate variable names should be exactly longitude and latitude")
+  }
+  if(is.data.frame(coordinatefile)){
+    coordinates(coordinatefile) = ~longitude + latitude
+    warning("coordinate variable names should be exactly longitude and latitude")
+  }
+
   testoverra = over(IDfile,coordinatefile)
-  testsp= spCbind(IDfile, testoverra)
-  testsf = as(testsp,"sf")
-  Stestsf = testsf[,c("X",variable)]
+  testsp= data.frame(spCbind(IDfile, testoverra))
+
+  Stestsf = testsp[,c("X", varname)]
+  names(Stestsf)=c("X", newname)
 
   mergedfile = merge(file2merge, Stestsf, by.x = "ID", by.y = "X", all.x = TRUE)
   return(mergedfile)
