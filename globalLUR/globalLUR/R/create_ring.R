45d0e76aad00b4e091d2e8a87b9e42db39db88dc
Lu <m.lu@uu.nl>
2018-11-30 11:58:05 +0100

    making create ring more generic

--- globalLUR/globalLUR/R/create_ring.R
+++ globalLUR/globalLUR/R/create_ring.R
@@ -4,32 +4,41 @@
 #' @return the road buffers becomes road ring buffers
 #' @export
 
-create_ring = function(inde_var,normalize=F)
+create_ring = function(inde_var,normalize=F, buffers_in = c(0,25,50,100,300,500,1000),
+                       buffers_out = c(25, 50,100,300,500,1000,5000),number_roadtypes)
+
 {
-  roadsonly =  inde_var[, which(grepl("ROAD",dimnames( inde_var)[[2]]))]
-  for5000m = seq(7, 35, by = 7)
-  roadsonly_in = roadsonly
-  roadsonly_in[,for5000m] = 0 # don't need 5000, so let them be 0 and put them in front, for road ring 0.
-  othervar = inde_var[, -which(grepl("ROAD",dimnames( inde_var)[[2]]))]
+  buffernumber = length(buffers_in)
+  roadsonly =  inde_var[, which(grepl("ROAD", names( inde_var) ))]
+ # for5000m = seq(buffernumber,  number_roadtypes*buffernumber, by = buffernumber)
+ # roadsonly_in = roadsonly
+ # roadsonly_in[,for5000m] = 0 # don't need 5000, so let them be 0 and put them in front, for road ring 0.
+  othervar = inde_var[, -which(grepl("ROAD", names( inde_var) ))]
   #if(is.null(names(othervar)))
   #names(othervar) = "VAR1"
   #roadsonlyin_s = roadsonly_in [,c(7,1:6,14, 8:13, 21, 15:20, 28, 22:27)] #shuffel, 0 is in the beginning, for buffer1
+ # roadsonlyin_s = roadsonly_in [,c(for5000m[1],1:6,for5000m[2], 8:13, for5000m[3], 15:20 )]
+  #roadsonlyin_s = roadsonly_in [,c(7,1:6,14, 8:13, 21, 15:20, 28, 22:27,35, 29:34)]
+  # roadrings = roadsonly - roadsonlyin_s
+  getring=function(i, roadsonly, bf = buffernumber)
+   {
+    roadtype_n = roadsonly[((i-1)*bf+1) :(i*bf)]
 
-  roadsonlyin_s = roadsonly_in [,c(7,1:6,14, 8:13, 21, 15:20, 28, 22:27,35, 29:34)]
+   roadring_n =  cbind(roadtype_n[,1],t(apply(roadtype_n, 1, diff)))
+ }
+ roadr= data.frame(lapply(1:number_roadtypes, getring, roadsonly))
 
-  buffers1 = c(0,25,50,100,300,500,1000)
-  buffers2 = c(25, 50,100,300,500,1000,5000)
-  roadrings = roadsonly - roadsonlyin_s
-  ringnames = paste(dimnames(roadrings)[[2]], buffers1,sep = "_")
-  dimnames(roadrings)[[2]] = ringnames
-  roadrings = data.frame(cbind(othervar, roadrings))
+  ringnames = paste(names(roadsonly) , buffers_in,sep = "_")
+  names(roadr ) = ringnames
+
+   roadrings = data.frame(cbind(othervar, roadr ))
 
 
   if (normalize )
   {
     area = function(r)  pi*(r^2)
 
-    arearing = sapply(buffers2, area)- sapply(buffers1, area)
+    arearing = sapply(buffers_out, area)- sapply(buffers_in, area)
 
     nor_ring = sweep(roadrings,2, t(rep(arearing,5)), "/")
   }
