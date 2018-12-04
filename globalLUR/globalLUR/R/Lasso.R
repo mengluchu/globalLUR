34bc03acfc6371018ccdd82a84d5179a9a03cc3e
Lu <m.lu@uu.nl>
2018-12-04 13:59:55 +0100

    plot cvfit

--- globalLUR/globalLUR/R/Lasso.R
+++ globalLUR/globalLUR/R/Lasso.R
@@ -19,6 +19,7 @@ Lasso = function (variabledf, y_varname= c("day_value","night_value", "value_mea
 
   cvfit <- glmnet::cv.glmnet(as.matrix(pre_mat_tr),y_tr_value ,type.measure = "mse",standardize=TRUE,alpha = 0.5,lower.limit=0)
  # print( coef(cvfit))
+  plot(cvfit)
   Lassoselected(cvfit)
 
   elastic_pred = predict(cvfit, newx=as.matrix(pre_mat_test))
