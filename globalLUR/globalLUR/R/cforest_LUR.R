5ad6b989f7e0258896fc3c2f68dfa2c37009d92a
Lu <m.lu@uu.nl>
2019-03-05 12:11:43 +0100

    ctree

---
+++ globalLUR/globalLUR/R/cforest_LUR.R
@@ -0,0 +1,43 @@
+#' Random forest for LUR (using cforest)
+
+#' @param  variabledf the dataframe containing predictors and dependent variable
+#' @param y_varname  name of the dependent variable.
+#' @param training the index for the rows used for training.
+#' @param test the index for the rows used for testing.
+#' @param grepstring the variable/column names of predictors in Lasso, grepl stlye, e.g. "ROAD|pop|temp|wind|Rsp|OMI|eleva|coast"
+#' @return  plot variable importance and an error matrix
+#' @export
+
+
+cforest_LUR = function (variabledf, y_varname= c("day_value","night_value", "value_mean"), training, test,  grepstring ="ROAD|pop|temp|wind|Rsp|OMI|eleva|coast", ...)
+{
+  prenres = paste(y_varname,"|", grepstring, sep = "")
+  pre_mat = subset_grep(variabledf[training,], prenres)
+
+
+  y_test = variabledf[test, y_varname]
+  x_test = variabledf[test,  ]
+
+  formu = as.formula(paste(y_varname,"~.", sep = ""))
+
+  rf3 <- cforest(formu , data = pre_mat )
+
+  df = data.frame(imp_val  = varimp(rf3))
+
+  imp_plot = ggplot(df, aes(x=reorder(rownames(df), imp_val), y=imp_val,fill=imp_val))+
+    geom_bar(stat="identity", position="dodge")+ coord_flip()+
+    ylab("Variable Importance")+
+    xlab("")+
+    ggtitle(paste("Information Value Summary", y_varname, sep =": "))+
+    guides(fill=F)+
+    scale_fill_gradient(low="red", high="blue")
+
+  print(imp_plot)
+
+  pre_rf <- predictions(predict(rf3, data =x_test  ))
+  #rf_residual <- pre_rf -  rdf_test$NO2
+
+  print(error_matrix(y_test, pre_rf))
+
+
+}
--- globalLUR/globalLUR/R/ctree_LUR.R
+++ globalLUR/globalLUR/R/ctree_LUR.R
@@ -6,16 +6,28 @@
 #' @param grepstring the variable/column names of predictors in Lasso, grepl stlye, e.g. "ROAD|pop|temp|wind|Rsp|OMI|eleva|coast"
 #' @return plot the tree and return the ctree object
 #' @export
-ctree_LUR = function (variabledf, y_varname= c("day_value","night_value", "value_mean"), training, grepstring ="ROAD|pop|temp|wind|Rsp|OMI|eleva|coast", ...)
+ctree_LUR = function (variabledf, y_varname= c("day_value","night_value", "value_mean"), training, test, grepstring ="ROAD|pop|temp|wind|Rsp|OMI|eleva|coast", ...)
 {
+
   prenres = paste(y_varname,"|", grepstring, sep = "")
   pre_mat = subset_grep(variabledf[training,], prenres)
 
+
+  y_test = variabledf[test, y_varname]
+  x_test = variabledf[test,  ]
+
   formu = as.formula(paste(y_varname,"~.", sep = ""))
 
-  cf = ctree(formu, data=pre_mat)
+  cf = ctree(formu,  data=pre_mat)
+
+
+  pre_rf <-  predict(cf, newdata =x_test  )
+  #rf_residual <- pre_rf -  rdf_test$NO2
+
+  print(error_matrix(y_test, pre_rf))
+
   print( plot(cf,fitmean = T)) #ctree party
-  return(cf)
+ # return(cf)
 #  cf2=rpart(formu, data=pre_mat)
 
 }
