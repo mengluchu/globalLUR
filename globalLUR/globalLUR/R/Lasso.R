163bc26ba581b01189e3bdcc727d7b297b06683d
Lu <m.lu@uu.nl>
2019-03-15 10:59:58 +0100

    ridge lasso

--- globalLUR/globalLUR/R/Lasso.R
+++ globalLUR/globalLUR/R/Lasso.R
@@ -9,7 +9,7 @@
 #' @export
 
 
-Lasso = function (variabledf, vis1 = T, y_varname= c("day_value","night_value", "value_mean"), training, test, grepstring ="ROAD|pop|temp|wind|Rsp|OMI|eleva|coast")
+Lasso = function (variabledf, vis1 = T, alpha = 1, y_varname= c("day_value","night_value", "value_mean"), training, test, grepstring ="ROAD|pop|temp|wind|Rsp|OMI|eleva|coast")
 {
   pre_mat = subset_grep(variabledf, grepstring)
   pre_mat_tr = pre_mat[training,]
@@ -17,8 +17,9 @@ Lasso = function (variabledf, vis1 = T, y_varname= c("day_value","night_value", 
   y_tr_value = variabledf[training, y_varname]
   y_test_value = variabledf[test,y_varname]
 
-  cvfit <- glmnet::cv.glmnet(as.matrix(pre_mat_tr),y_tr_value ,type.measure = "mse",standardize=TRUE,alpha = 0.5,lower.limit=0)
+  cvfit <- glmnet::cv.glmnet(as.matrix(pre_mat_tr),y_tr_value ,type.measure = "mse",standardize=TRUE,alpha = alpha,lower.limit=0)
  # print( coef(cvfit))
+  print(cvfit)
   if ( vis1   ){
    plot(cvfit)
   Lassoselected(cvfit)
