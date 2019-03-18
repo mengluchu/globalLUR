ffdedc347e9268fe7fd071df92af3b4db0866854
Lu <m.lu@uu.nl>
2019-03-18 17:04:00 +0100

    a

--- globalLUR/globalLUR/R/Brt_LUR.R
+++ globalLUR/globalLUR/R/Brt_LUR.R
@@ -9,25 +9,36 @@
 #' @export
 
 
-Brt_LUR = function (variabledf, vis1 = T, y_varname= c("day_value","night_value", "value_mean"), training, test,  grepstring ="ROAD|pop|temp|wind|Rsp|OMI|eleva|coast", ...)
+Brt_LUR = function (variabledf, opti = F, vis1 = T, ntree= 500, y_varname= c("day_value","night_value", "value_mean"), training, test,  grepstring ="ROAD|pop|temp|wind|Rsp|OMI|eleva|coast", ...)
 {
   prenres = paste(y_varname,"|", grepstring, sep = "")
   pre_mat = subset_grep(variabledf[training,], prenres)
 
-
+  Xmat = subset_grep(variabledf[training,], grepstring )
   y_test = variabledf[test, y_varname]
   x_test = variabledf[test,  ]
 
   formu = as.formula(paste(y_varname,"~.", sep = ""))
 
+ if(opti)
+  {
+   rf3 <- gbm.step(data=pre_mat, gbm.x=names(Xmat),gbm.y=y_varname, family="gaussian", tree.complexity = 6, learning.rate = 0.01, bag.fraction = 0.5)
+   ntree = rf3$gbm.call$best.trees
+   }
+  else
+    {
+      formu = as.formula(paste(y_varname,"~.", sep = ""))
+      rf3 =  gbm(formula = formu, data = pre_mat, distribution = "gaussian",
+             n.trees = 500,
+             interaction.depth = 6,  shrinkage = 0.01,
+             bag.fraction = 0.5 )
 
-  rf3 <- gbm.step(data=pre_mat, gbm.x=names(x_test),gbm.y=y_varname, family="gaussian", tree.complexity = 6, learning.rate = 0.01, bag.fraction = 0.5)
+    }
 
-  df = data.frame(imp_val  = varimp(rf3))
   if (vis1 ){
      gbm.plot(rf3)
   }
-  pre_rf <- predict.gbm( rf3, x_test ,n.trees=rf3$gbm.call$best.trees, type="response")
+  pre_rf <- predict.gbm( rf3, x_test ,n.trees=ntree, type="response")
   #rf_residual <- pre_rf -  rdf_test$NO2
 
   return(error_matrix(y_test, pre_rf))
