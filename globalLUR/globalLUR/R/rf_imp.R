66dd0aa17b89b277323e7ed37f1778adc94a7b60
Lu <m.lu@uu.nl>
2019-05-02 11:55:03 +0200

    i

--- globalLUR/globalLUR/R/rf_imp.R
+++ globalLUR/globalLUR/R/rf_imp.R
@@ -1,6 +1,6 @@
 #' return variable importance, RF
 #' @export
-rf_imp = function (variabledf, mtry = 33, num.trees=1000,  y_varname= c("day_value","night_value", "value_mean"), training, test,  grepstring ="ROAD|pop|temp|wind|Rsp|OMI|eleva|coast", ...)
+rf_imp = function (variabledf, mtry = 33, num.trees=2000,  y_varname= c("day_value","night_value", "value_mean"), training, test,  grepstring ="ROAD|pop|temp|wind|Rsp|OMI|eleva|coast", ...)
 {
   prenres = paste(y_varname,"|", grepstring, sep = "")
   pre_mat = subset_grep(variabledf[training,], prenres)
