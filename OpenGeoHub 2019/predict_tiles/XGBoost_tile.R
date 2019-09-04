032c8c505f201574ab6a7d6c5897adabc37987d5
Meng Lu <lumeng0312@gmial.com>
2019-09-05 00:28:49 +0200

    hands-on

--- OpenGeoHub 2019/predict_tiles/XGBoost_tile.R
+++ OpenGeoHub 2019/predict_tiles/XGBoost_tile.R
@@ -6,15 +6,15 @@ ipak <- function(pkg){
     install.packages(new.pkg, dependencies = TRUE, repos='http://cran.muenster.r-project.org')
   sapply(pkg, require, character.only = TRUE)
 }
-packages <- c( "raster", "dplyr",  "rgdal","Matrix","xgboost"  )
+packages <- c( "raster", "dplyr",  "rgdal","Matrix","xgboost", "data.table"  )
 ipak(packages)
 
 
-source("E:/OpenGeoHub 2019/tiles/GlobalLUR_R/sampledf.R")
+source("predict_tiles/GlobalLUR_R/sampledf.R")
 
-source("E:/OpenGeoHub 2019/tiles/GlobalLUR_R/subset_grep.R")
-source("E:/OpenGeoHub 2019/tiles/GlobalLUR_R/merge_roads.R")
-load("E:/OpenGeoHub 2019/tiles/data/mergedu.Rdata")
+source("predict_tiles/GlobalLUR_R/subset_grep.R")
+source("predict_tiles/GlobalLUR_R/merge_roads.R")
+load("predict_tiles/data/mergedu.Rdata")
 
 COUN =  "de"
 
@@ -44,27 +44,28 @@ a=  sampledf(merged,fraction = 1, country2digit = toupper(COUN))
 
 
 # merge raster roads
-    lf  = list.files(paste0("E:/OpenGeoHub 2019/tiles/", toupper(COUN), "/wgs84"), pattern = "*.tif$", full.names = T)
+    lf  = list.files(paste0("predict_tiles/", toupper(COUN), "/wgs84"), pattern = "*.tif$", full.names = T)
     lf  = lf[-which(grepl("road_class_3|road_class_4|road_class_5", lf))]
-    lfr3  = list.files(paste0("E:/OpenGeoHub 2019/tiles/", toupper(COUN), "/wgs84"), pattern = "road_class_3_.*.tif$", full.names = T)
+    lfr3  = list.files(paste0("predict_tiles/", toupper(COUN), "/wgs84"), pattern = "road_class_3_.*.tif$", full.names = T)
     sr3 = stack(lfr3)
-    lfr4  = list.files(paste0("E:/OpenGeoHub 2019/tiles/", toupper(COUN), "/wgs84"), pattern = "road_class_4_.*.tif$", full.names = T)
+    lfr4  = list.files(paste0("predict_tiles/", toupper(COUN), "/wgs84"), pattern = "road_class_4_.*.tif$", full.names = T)
     sr4 = stack(lfr4)
-    lfr5  = list.files(paste0("E:/OpenGeoHub 2019/tiles/", toupper(COUN), "/wgs84"), pattern = "road_class_5_.*.tif$", full.names = T)
+    lfr5  = list.files(paste0("predict_tiles/", toupper(COUN), "/wgs84"), pattern = "road_class_5_.*.tif$", full.names = T)
     sr5 = stack(lfr5)
-    sr345 = sr3+sr4+sr5 # aggregate roads 345 and the name is 3
-
-    s1 = stack(lf,sr345)
-
-#merge roads
+    sr345 = sr3 + sr4 + sr5 # aggregate roads 345 
+    
+    #merge roads
     inde_var = merge_roads(inde_var ,c(3, 4, 5), keep = F)
-#match names
-    names(inde_var) =  gsub("M345", "3", names(inde_var)) # note 3 is for M345
-
-# select variables
+    #match names
+    names(sr345) = names(subset_grep(inde_var, "road_class_M345"))
+    s1 = stack(lf, sr345) 
+    
+    # select variables
     pre_mat3 = subset_grep(inde_var, "road|temperature|wind|pop|ele|Rsp|rop|OMI|industry")
-# reorder the dataframe
-    pre_mat3 %>%dplyr::select (names(s1)) -> pre_mat3
+    # reorder the dataframe! seems lasso only match the matrix
+    pre_mat3 %>% select (names(s1)) -> pre_mat3
+    # make sure the nams match!   
+    all.equal(names(s1), names(pre_mat3))
 
     formu = as.formula(paste("value", "~.", sep = ""))
 
@@ -85,12 +86,13 @@ a=  sampledf(merged,fraction = 1, country2digit = toupper(COUN))
     snight = predict(s1, bst, fun = predfun)
 
 
-
-    dirout1 = paste0("E:/OpenGeoHub 2019/tiles/GlobalLUR_R/output/")
+    plot(snight)
+    plot(sday)
+    dirout1 = paste0("predict_tiles/output/")
     name1 = paste0( dirout1,  COUN , "xgbday.tif")
 
     name2 = paste0( dirout1,  COUN , "xgbnight.tif")
-    writeRaster(sday, name1, overwrite=TRUE )
-    writeRaster(snight, name2, overwrite=TRUE )
+    writeRaster(sday, name1, overwrite = TRUE )
+    writeRaster(snight, name2, overwrite = TRUE )
 
 
